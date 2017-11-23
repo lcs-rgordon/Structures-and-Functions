f2fbf066e1e6773e0d289e6b1aac344d8f67a188
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-11-23 16:47:54 -0500

    Solution to exercise

--- Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -32,4 +32,101 @@ import Foundation
 
 */
 
-// Begin here... 
+// Define the Point struct
+struct Point {
+    var x : Double = 0.0
+    var y : Double = 0.0
+}
+
+// Define the Slope struct
+struct Slope {
+    var rise : Double = 1.0
+    var run : Double = 1.0
+}
+
+// Define the Line struct
+struct Line {
+    var slope : Slope = Slope()
+    var verticalIntercept : Double = 5.0
+}
+
+/// Returns the negative perpendicular slope when given a line.
+///
+/// - Parameter from: The line that we are starting from.
+/// - Returns: The slope of a line that would be perpendicular to the given line.
+func getSlopeOfPerpendicularLine(from givenLine: Line) -> Slope {
+    
+    return Slope(rise: givenLine.slope.run, run: givenLine.slope.rise * -1)
+    
+}
+
+/// Get the vertical intercept for a line given a point on the line and it's slope
+///
+/// - Parameters:
+///   - p: a point on the line
+///   - m: the slope of the line
+/// - Returns: the vertical intercept of the line
+func getVerticalIntercept(from p: Point, onLineWith m: Slope) -> Double {
+    
+    let b = p.y + (m.rise / m.run) * p.x * -1       // b = y - m * x
+    return b
+    
+}
+
+/// Get the point where two lines intersect on a Cartesian plane
+///
+/// - Parameters:
+///   - between: the first line
+///   - and: the second line
+/// - Returns: the point of intersection
+func getPointOfIntersection(between first: Line, and second: Line) -> Point {
+    
+    // Get the sum of the co-efficients of the variable terms (the sum of the slopes)
+    let variableTermSum = second.slope.rise / second.slope.run + first.slope.rise / first.slope.run * -1
+    
+    // Get the sum of the vertical intercepts
+    let constantTermSum = first.verticalIntercept + second.verticalIntercept * -1
+    
+    // Get the abscissa (x-value)
+    let abscissa = constantTermSum / variableTermSum
+    
+    // Now get the ordinate (y-value) by "substituting" into the second equation
+    let ordinate = second.slope.rise / second.slope.run * abscissa + second.verticalIntercept
+    
+    // Return the point
+    return Point(x: abscissa, y: ordinate)
+}
+
+/// Finds the distance between two points on a Cartesian plane
+///
+/// - Parameters:
+///   - from: The first point
+///   - to: The second point
+/// - Returns: The distance between these two points
+func distance(from: Point, to: Point) -> Double {
+    
+    return sqrt(pow(from.x - to.x,2) + pow(from.y - to.y, 2))
+    
+}
+
+// Create a line instance (representing the road in this example)
+let m = Slope(rise: -1, run: 2)
+let existingRoad = Line(slope: m, verticalIntercept: 9.5)
+
+// Create a point (the cabin site in this example)
+let cabinSite = Point(x: 6.0, y: 1.5)
+
+// Get the slope of a line perpendicular to the road
+let perpendicularM = getSlopeOfPerpendicularLine(from: existingRoad)
+
+// Get the vertical intercept of a line perpendicular to the existing road
+let perpendicularB = getVerticalIntercept(from: cabinSite, onLineWith: perpendicularM)
+
+// Define the line perpendicular to the existing road
+let newRoad = Line(slope: perpendicularM, verticalIntercept: perpendicularB)
+
+// Find the point of intersection between the two roads
+let intersectionOfRoads = getPointOfIntersection(between: existingRoad, and: newRoad)
+
+// Now use the distance function to find the shortest route
+let shortestDistance = distance(from: cabinSite, to: intersectionOfRoads)
